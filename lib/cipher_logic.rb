4d178f4f36138ac59647dc00d97866d75ce510b8
petumazo <davidalvarezmazzo@gmail.com>
2021-05-08 16:38:16 -0500

    Correct tubocop errors

--- lib/cipher_logic.rb
+++ lib/cipher_logic.rb
@@ -8,12 +8,12 @@ def caesar_cipher(str, caesar_num)
   end
 end
 
-def caesar_decipher (str, caesar_num)
-    str.each_codepoint do |char|
-        if char - caesar_num >= 32
-          print (char - caesar_num).chr
-        else
-          print (126 - (char + caesar_num)).chr
-        end
-      end
+def caesar_decipher(str, caesar_num)
+  str.each_codepoint do |char|
+    if char - caesar_num >= 32
+      print (char - caesar_num).chr
+    else
+      print (126 - (char + caesar_num)).chr
+    end
+  end
 end
--- main.rb
+++ main.rb
@@ -7,32 +7,32 @@ puts
 repeat = false
 
 until repeat
-    puts 'Please type ´code´ if you want to CIPHER or ´decode´ if you want to DECIPHER'
-    action = gets.chomp
-    if action.downcase == "code"
-        puts 'Enter the word/words you want to cipher'
-        str = gets.chomp
-        puts 'Enter the cipher number'
-        num = gets.chomp.to_i
-        puts "After making a complex cipher process, this is the result:" 
-        caesar_cipher(str,num)
-        clear
-        puts 'If you want to cipher or decipher again please type ´again´'
-        again = gets.chomp
-        again == 'again' ? repeat = false : repeat = true
-    elsif action.downcase == "decode"
-        puts 'Enter the word/words you want to decode'
-        str = gets.chomp
-        puts 'Enter the cipher number'
-        num = gets.chomp.to_i
-        puts "After making a complex decipher process, this is the result:"
-        caesar_decipher(str,num)
-        clear
-        puts 'If you want to cipher or decipher again please type ´again´'
-        again = gets.chomp
-        again == 'again' ? repeat = false : repeat = true
-    else
-        repeat = false
-    end
+  puts 'Please type ´code´ if you want to CIPHER or ´decode´ if you want to DECIPHER'
+  action = gets.chomp
+  case action.downcase
+  when 'code'
+    puts 'Enter the word/words you want to cipher'
+    str = gets.chomp
+    puts 'Enter the cipher number'
+    num = gets.chomp.to_i
+    puts 'After making a complex cipher process, this is the result:'
+    caesar_cipher(str, num)
+    clear
+    puts 'If you want to cipher or decipher again please type ´again´'
+    again = gets.chomp
+    repeat = again != 'again'
+  when 'decode'
+    puts 'Enter the word/words you want to decode'
+    str = gets.chomp
+    puts 'Enter the cipher number'
+    num = gets.chomp.to_i
+    puts 'After making a complex decipher process, this is the result:'
+    caesar_decipher(str, num)
+    clear
+    puts 'If you want to cipher or decipher again please type ´again´'
+    again = gets.chomp
+    repeat = again != 'again'
+  else
+    repeat = false
+  end
 end
-
